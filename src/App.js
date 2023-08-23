b1c6a5c01659941d77672e1cd20b71ebe58c3a46
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-08-23 14:01:37 +0530

    contact us section complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import "./App.css";
+import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Ravi.css";
-import "./Sukhvinder.css";
-import "./Mayank.css";
+import './Ravi.css'
+import './Sukhvinder.css'
+import './Mayank.css'
+import ContactUs from './components/ContactUs';
 
 function App() {
-  return
-  <>
-    
-  </>
-    ;
+  return (
+    <>
+      <div className='overflow-hidden bg_body'>
+        <ContactUs />
+      </div>
+    </>
+  );
 }
 
 export default App;
