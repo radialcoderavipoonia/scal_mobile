76fb5547d2c1c514dd5a777b3fc4b209765f9340
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-08-23 16:21:17 +0530

    slider

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import "./Sukhvinder.css";
 import "./Mayank.css";
 import MyNav from "./components/MyNav";
 import ContactUs from './components/ContactUs';
+import Services from './components/Services';
+// or other themes
+// import '@splidejs/react-splide/css/skyblue';
+import '@splidejs/react-splide/css/sea-green';
 
 function App() {
   return (
@@ -12,6 +16,7 @@ function App() {
       <div className='overflow-hidden bg_body'>
         <MyNav/>
         <ContactUs />
+        <Services/>
       </div>
     </>
   );
