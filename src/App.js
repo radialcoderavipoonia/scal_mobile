7c637d18a392f158588b09f84480c39b5edffd09
sukhvinder-jaat <monudhaka829@gmail.com>
2023-08-23 14:04:06 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./Ravi.css";
 import "./Sukhvinder.css";
 import "./Mayank.css";
+import MyNav from "./components/MyNav";
 
 function App() {
-  return
-  <>
-    
-  </>
-    ;
+  return (
+    <>
+      <div className=" bg_body overflow-hidden">
+        <MyNav />
+      </div>
+    </>
+  );
 }
-
 export default App;
